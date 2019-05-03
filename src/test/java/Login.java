7bdf60da7f366be31d561a9a2a2b1eb4e5dd2c1f
attiburcio <ariadne.tamires@dextra-sw.com>
2019-05-03 14:23:27 -0300

    teste

---
+++ src/test/java/Login.java
@@ -0,0 +1,25 @@
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class Login {
+
+
+
+
+
+
+
+
+
+
+}
